78dd074fc2040013158db49620459a21eff8ccd2
Duamelo <david.dosseh@imsp-uac.org>
2020-08-16 14:30:16 +0100

    chargement initial

---
+++ index.js
@@ -0,0 +1,18 @@
+var async = require("./lib/async.js");
+var request = require("./lib/request.js");
+
+var config = require("./config.json");
+
+ 
+
+ async.map(config.images, request, function(err, results){
+        if(err)
+            console.error(err);
+        var totalSize = 0;
+        for (var i = 0 ; i < results.length; i++){
+            totalSize += results[i].length;
+        }
+        console.log("All Download ended, results :", totalSize);
+ });
+
+ 
